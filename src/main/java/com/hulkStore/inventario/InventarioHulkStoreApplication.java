6715f9eaa282fbb06308c29dbf224bf63b296a3a
Leticia Villa <lettyirene.villa.lopez@gmail.com>
2022-04-29 22:55:12 -0500

    Init Comit
    Init Comit

---
+++ src/main/java/com/hulkStore/inventario/InventarioHulkStoreApplication.java
@@ -0,0 +1,13 @@
+package com.hulkStore.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioHulkStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioHulkStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hulkStore/inventario/InventarioHulkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulkStore.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioHulkStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
